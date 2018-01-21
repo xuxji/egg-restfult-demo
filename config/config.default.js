09d161365e8b6d774da3f7c77c65c1f9b40a0ea0
xiaoji.xxj <xiaoji.xxj@alibaba-inc.com>
2018-01-21 22:14:14 +0800

    feat: update

--- config/config.default.js
+++ config/config.default.js
@@ -20,25 +20,5 @@ module.exports = appInfo => {
   // add your config here
   config.middleware = [];
 
-  config.mysql = {
-    // 单数据库信息配置
-    client: {
-      // host
-      host: 'localhost',
-      // 端口号
-      port: '3306',
-      // 用户名
-      user: 'root',
-      // 密码
-      password: '3336481',
-      // 数据库名
-      database: 'egg-demo',
-    },
-    // 是否加载到 app 上，默认开启
-    app: true,
-    // 是否加载到 agent 上，默认关闭
-    agent: false,
-  };
-
   return config;
 };
