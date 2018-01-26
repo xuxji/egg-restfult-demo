2d794987ce4fd16d3ddc4b7a47d780220958b14d
xiaoji.xxj <xiaoji.xxj@alibaba-inc.com>
2018-01-26 14:12:40 +0800

    update

--- config/config.prod.js
+++ config/config.prod.js
@@ -10,13 +10,13 @@ module.exports = appInfo => {
     // 单数据库信息配置
     client: {
       // host
-      host: '120.77.13.90',
+      host: 'host',
       // 端口号
       port: '3306',
       // 用户名
       user: 'root',
       // 密码
-      password: '123456',
+      password: 'root',
       // 数据库名
       database: 'tedit',
     },
